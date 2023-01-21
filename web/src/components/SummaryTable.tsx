6345088c451f634a374e237139bf1acbbcada4ed
Thigo <thiago.l17@yahoo.com>
2023-01-21 11:36:40 -0300

    feat(web):refactor

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -1,5 +1,5 @@
-import { generateDatesFromYearBeginning } from "../utils/generate-dates-from-year-beginning"
-import { HabitDay } from "./HabitDay"
+import { generateDatesFromYearBeginning } from '../utils/generate-dates-from-year-beginning'
+import { HabitDay } from './HabitDay'
 
 const weekDays = [
   'D',
@@ -13,18 +13,18 @@ const weekDays = [
 
 const summaryDates = generateDatesFromYearBeginning();
 
-const minimumSumaryDatesSize = 18 * 7 // 18weeks
-const amountOfDaysToFill = minimumSumaryDatesSize - summaryDates.length
+const minimumSummaryDatesSize = 18 * 7 // 18weeks
+const amountOfDaysToFill = minimumSummaryDatesSize - summaryDates.length
 
 export function SummaryTable() {
   return (
-      <div className="w-full flex">
-        <div className="grid grid-rows-7 grid-flow-row gap-3">
+      <div className='w-full flex'>
+        <div className='grid grid-rows-7 grid-flow-row gap-3'>
           {weekDays.map((weekDay, i) => {
               return (
                   <div
                   key={`${weekDay}-${i}`} 
-                  className="text-zinc-400 text-xl h-10 w-10 font-bold flex items-center justify-center"
+                  className='text-zinc-400 text-xl h-10 w-10 font-bold flex items-center justify-center'
                   >
                   {weekDay}
                 </div>
@@ -32,7 +32,7 @@ export function SummaryTable() {
           })}
         </div>
 
-        <div className="grid grid-rows-7 grid-flow-col gap-3">
+        <div className='grid grid-rows-7 grid-flow-col gap-3'>
           {summaryDates.map(date => {
             return <HabitDay key={date.toString()} />
           })}
@@ -41,7 +41,7 @@ export function SummaryTable() {
             return (
               <div 
                 key={i} 
-                className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed" 
+                className='w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed' 
               />
             )
         })}
