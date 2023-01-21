28efbfef4642069f687f780ff346851ddc3e7f78
Thigo <thiago.l17@yahoo.com>
2023-01-21 11:34:01 -0300

    feat(server):refactor

--- server/prisma/seed.ts
+++ server/prisma/seed.ts
@@ -1,19 +1,19 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
-const firstHabitId = '0730ffac-d039-4194-9571-01aa2aa0efbd'
-const firstHabitCreationDate = new Date('2022-12-31T03:00:00.000')
+const firstHabitId = '0730ffac-d039-4194-9571-01aa2aa0efbd';
+const firstHabitCreationDate = new Date('2022-12-31T03:00:00.000');
 
-const secondHabitId = '00880d75-a933-4fef-94ab-e05744435297'
-const secondHabitCreationDate = new Date('2023-01-03T03:00:00.000')
+const secondHabitId = '00880d75-a933-4fef-94ab-e05744435297';
+const secondHabitCreationDate = new Date('2023-01-03T03:00:00.000');
 
-const thirdHabitId = 'fa1a1bcf-3d87-4626-8c0d-d7fd1255ac00'
-const thirdHabitCreationDate = new Date('2023-01-08T03:00:00.000')
+const thirdHabitId = 'fa1a1bcf-3d87-4626-8c0d-d7fd1255ac00';
+const thirdHabitCreationDate = new Date('2023-01-08T03:00:00.000');
 
 async function run() {
-  await prisma.habit.deleteMany()
-  await prisma.day.deleteMany()
+  await prisma.habit.deleteMany();
+  await prisma.day.deleteMany();
 
   /**
    * Create habits
@@ -65,7 +65,7 @@ async function run() {
         }
       }
     })
-  ])
+  ]);
 
   await Promise.all([
     /**
@@ -113,8 +113,8 @@ async function run() {
         }
       }
     }),
-  ])
-}
+  ]);
+};
 
 run()
   .then(async () => {
@@ -124,4 +124,5 @@ run()
     console.error(e)
     await prisma.$disconnect()
     process.exit(1)
-  })
+  });
+  
