adf94d4f4e2102af82e9db21e64951ef3d7e222f
Thigo <thiago.l17@yahoo.com>
2023-01-22 09:43:59 -0300

    feat(mobile): conecting with API backend

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,8 +1,12 @@
+import { useState, useEffect } from 'react';
 import { useNavigation } from '@react-navigation/native';
-import { View, Text, ScrollView } from 'react-native';
+import { View, Text, ScrollView, Alert } from 'react-native';
+
+import { api } from '../lib/axios';
+import { generateDatesFromYearBeginning } from '../utils/generate-dates-from-year-beginning';
+
 import { HabitDay, DAY_SIZE } from '../components/HabitDay';
 import { Header } from '../components/Header';
-import { generateDatesFromYearBeginning } from '../utils/generate-dates-from-year-beginning';
 
 const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'];
 
@@ -11,7 +15,29 @@ const minimumSummaryDateSizes = 18 * 5;
 const amountOfDaysToFill = minimumSummaryDateSizes - datesFromYourStart.length;
 
 export function Home() {
+  const [loading, setLoading] = useState(true);
+  const [summary, setSummary] = useState(null);
+
   const { navigate } = useNavigation();
+
+  async function fetchData() {
+    try {
+      setLoading(true);
+      const response = await api.get('/summary');
+      console.log(response.data);
+      setSummary(response.data);
+
+    } catch (error) {
+      Alert.alert('Ops', 'Não foi possível carregador o sumário de hábitos.');
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
     <View className='flex-1 bg-background px-8 pt-16'>
