b440b4cd83a51eb4ee2251120481a48afefa8d83
Thigo <thiago.l17@yahoo.com>
2023-01-21 22:18:10 -0300

    feat(web): handle form for create new habits

--- web/src/components/NewHabitForm.tsx
+++ web/src/components/NewHabitForm.tsx
@@ -1,5 +1,6 @@
 import * as Checkbox from '@radix-ui/react-checkbox';
 import { Check } from 'phosphor-react';
+import { FormEvent, useState } from 'react';
 
 const availableWeekDays = [
   'Domingo',
@@ -12,10 +13,32 @@ const availableWeekDays = [
 ];
 
 export function NewHabitForm() {
+  const [ title, setTitle ] = useState('');
+  const [ weekDays, setWeekDays ] = useState<number[]>([]);
+
+  function createNewHabit(event: FormEvent) {
+    event.preventDefault();
+
+    console.log(title, weekDays)
+  }
+
+  function handleToggleWeekDay(weekDay: number) {
+    if (weekDays.includes(weekDay)) {
+      const weekDaysWithRemoveOne = weekDays.filter(day => day !== weekDay)
+
+      setWeekDays(weekDaysWithRemoveOne)
+    } else {
+      const weekDaysWithAddedOne = [...weekDays, weekDay]
+
+      setWeekDays(weekDaysWithAddedOne)
+    }
+  } 
+
   return (
     <form 
       className='w-full flex flex-col mt-6'
-      action=''
+      onSubmit={createNewHabit}
+      
     >
       <label 
         htmlFor='title' 
@@ -30,6 +53,7 @@ export function NewHabitForm() {
         placeholder='ex.: Exercícios, tomar água, etc...'
         className='p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400'
         autoFocus
+        onChange={event =>  setTitle(event.target.value)}
       />
 
       <label 
@@ -40,24 +64,24 @@ export function NewHabitForm() {
       </label>
 
       <div className='flex flex-col gap-2 mt-3'>
-        {availableWeekDays.map(weekDay => {
+        {availableWeekDays.map((weekDay, index) => {
           return (
+            <Checkbox.Root
+              key={weekDay}
+              className='flex items-center gap-3 group' 
+              onCheckedChange={() => handleToggleWeekDay(index)}
+            >
+            <div className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500'>
+              <Checkbox.Indicator>
+                <Check size={20} className='text-white' />
+                </Checkbox.Indicator>
+            </div>
 
-        <Checkbox.Root
-          key={weekDay}
-          className='flex items-center gap-3 group'
-        >
-          <div className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500'>
-            <Checkbox.Indicator>
-              <Check size={20} className='text-white' />
-              </Checkbox.Indicator>
-          </div>
-
-          <span 
-            className='text-white leading-tight'>
-            {weekDay}   
-          </span>
-        </Checkbox.Root>
+            <span 
+              className='text-white leading-tight'>
+              {weekDay}   
+            </span>
+            </Checkbox.Root>
           )
         })}
       </div>
@@ -65,7 +89,6 @@ export function NewHabitForm() {
       <button 
         type='submit'
         className='mt-6 rounded-lg p-4 flex items-center justify-center gap-3 font-semibold bg-green-600 hover:bg-green-500'
-
       >
         <Check size={20} weight='bold'/>
           Confirmar
